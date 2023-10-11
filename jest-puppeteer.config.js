494cb36daa9c2f9d987cf88dfa637d00e996eaf8
jingming295 <jingming295@gmail.com>
2023-10-12 00:35:17 +0800

    📝更新版本号

--- jest-puppeteer.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    launch: {
-      headless: false,
-      product: 'firefox',
-      defaultViewport : {width: 1700, height: 800 }
-    },
-    browserContext: 'default',
-  }
