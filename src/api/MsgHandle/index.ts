c3c42f06fe802a494c06e2f8810c33cb74a076c8
jingming295 <jingming295@gmail.com>
2023-10-15 05:18:07 +0800

    🛠️ Modified: MsgHandle/index.ts

--- src/api/MsgHandle/index.ts
+++ src/api/MsgHandle/index.ts
@@ -19,6 +19,7 @@ export function apply(ctx: Context, config: Config)
             async ({ session, options }, ...rest: string[]): Promise<void> =>
             {
                 if (!session || !session.username || !options) return;
+
                 const username = session.username;
                 const uid = session.uid;
 
@@ -28,15 +29,17 @@ export function apply(ctx: Context, config: Config)
         );
 }
 
-async function handleMediaRequests(
-    handler: MediaHandler,
-    options: Record<string, boolean>,
-    rest: string[],
-    username: string,
-    uid: string,
-    session: Session,
-    config: Config
-): Promise<void>
+/**
+ * 处理媒体请求的函数
+ * @param handler 媒体处理器对象
+ * @param options 选项对象
+ * @param rest 剩余参数（媒体链接）
+ * @param username 用户名
+ * @param uid 用户ID
+ * @param session 会话对象
+ * @param config 配置对象
+ */
+async function handleMediaRequests(handler: MediaHandler, options: Record<string, boolean>, rest: string[], username: string, uid: string, session: Session, config: Config): Promise<void>
 {
     const logger = new Logger('iirose-media-request');
     try
@@ -76,6 +79,13 @@ async function handleMediaRequests(
     }
 }
 
+/**
+ * 处理'cut'选项的函数
+ * @param session 会话对象
+ * @param options 选项对象
+ * @param config 配置对象
+ * @param username 用户名
+ */
 function handleCutOption(session: Session, options: Record<string, boolean>, config: Config, username: string)
 {
     if (options['cut'] && session.platform === 'iirose')
@@ -85,15 +95,26 @@ function handleCutOption(session: Session, options: Record<string, boolean>, con
     }
 }
 
+/**
+ * 发送媒体消息的函数
+ * @param session 会话对象
+ * @param msg 消息信息对象
+ * @param config 配置对象
+ */
 function sendMediaMessage(session: Session, msg: msgInfo, config: Config)
 {
-    if (msg.mediaData !== null) {
+    if (msg.mediaData !== null)
+    {
+        const { name, url, signer, cover, duration, bitRate } = msg.mediaData;
+        const color = config['mediaCardColor'] || 'FFFFFF';
+
         if (msg.mediaData.type === 'music')
         {
-            session.send(`<audio name="${msg.mediaData.name}" url="${msg.mediaData.url}" author="${msg.mediaData.signer}" cover="${msg.mediaData.cover}" duration="${msg.mediaData.duration}" bitRate="${msg.mediaData.bitRate}" color="${config['mediaCardColor'] || 'FFFFFF'}"/>`);
+            session.send(`<audio name="${name}" url="${url}" author="${signer}" cover="${cover}" duration="${duration}" bitRate="${bitRate}" color="${color}"/>`);
         } else
         {
-            session.send(`<video name="${msg.mediaData.name}" url="${msg.mediaData.url}" author="${msg.mediaData.signer}" cover="${msg.mediaData.cover}" duration="${msg.mediaData.duration}" bitRate="${msg.mediaData.bitRate}" color="${config['mediaCardColor'] || 'FFFFFF'}"/>`);
+            session.send(`<video name="${name}" url="${url}" author="${signer}" cover="${cover}" duration="${duration}" bitRate="${bitRate}" color="${color}"/>`);
         }
     }
 }
+
