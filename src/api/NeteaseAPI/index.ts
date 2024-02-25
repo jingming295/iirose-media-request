0d5f4e8d2f281388965aef6b33f201d238bf61b7
jingming295 <jingming295@gmail.com>
2024-02-26 03:04:53 +0800

    Refactor: API headers

--- src/api/NeteaseAPI/index.ts
+++ src/api/NeteaseAPI/index.ts
@@ -111,14 +111,7 @@ export class NeteaseApi
 
 
         const response = await axios.post(url, params, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Linux; U; Android 8.1.0; zh-cn; BKK-AL10 Build/HONORBKK-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/10.6 Mobile Safari/537.36',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                Referer: 'https://music.163.com',
-                'X-Real-IP': '::1',
-                'X-Forwarded-For': '::1',
-                Cookie: 'osver=undefined; deviceId=undefined; appver=8.9.70; versioncode=140; mobilename=undefined; buildver=1697745346; resolution=1920x1080; __csrf=; os=android; channel=undefined; requestId=1697745346367_0886; MUSIC_A=1f5fa7b6a6a9f81a11886e5186fde7fb0b63372bce7ff361fa5cb1a86d5fbbbbadd2bc8204eeee5e04bf7bf7e7f4428eeb3a754c1a3a779110722d253c67f6e9fac900d7a89533ee3324751bcc9aaf44c3061cd18d77b7a0'
-            }
+            headers: this.returnHeaderV2()
         });
 
         if (response.status !== 200)
@@ -142,14 +135,7 @@ export class NeteaseApi
         });
 
         const response = await axios.post(url, params, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Linux; U; Android 8.1.0; zh-cn; BKK-AL10 Build/HONORBKK-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/10.6 Mobile Safari/537.36',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                Referer: 'https://music.163.com',
-                'X-Real-IP': '::1',
-                'X-Forwarded-For': '::1',
-                Cookie: 'osver=undefined; deviceId=undefined; appver=8.9.70; versioncode=140; mobilename=undefined; buildver=1697745346; resolution=1920x1080; __csrf=; os=android; channel=undefined; requestId=1697745346367_0886; MUSIC_A=1f5fa7b6a6a9f81a11886e5186fde7fb0b63372bce7ff361fa5cb1a86d5fbbbbadd2bc8204eeee5e04bf7bf7e7f4428eeb3a754c1a3a779110722d253c67f6e9fac900d7a89533ee3324751bcc9aaf44c3061cd18d77b7a0'
-            }
+            headers: this.returnHeaderV2()
         });
 
         if (response.status !== 200)
@@ -165,6 +151,18 @@ export class NeteaseApi
         return {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
             'Content-Type': 'application/json'
+        };
+    }
+
+    private returnHeaderV2()
+    {
+        return {
+            'User-Agent': 'Mozilla/5.0 (Linux; U; Android 8.1.0; zh-cn; BKK-AL10 Build/HONORBKK-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/10.6 Mobile Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Referer: 'https://music.163.com',
+            'X-Real-IP': '::1',
+            'X-Forwarded-For': '::1',
+            Cookie: 'osver=undefined; deviceId=undefined; appver=8.9.70; versioncode=140; mobilename=undefined; buildver=1697745346; resolution=1920x1080; __csrf=; os=android; channel=undefined; requestId=1697745346367_0886; MUSIC_A=1f5fa7b6a6a9f81a11886e5186fde7fb0b63372bce7ff361fa5cb1a86d5fbbbbadd2bc8204eeee5e04bf7bf7e7f4428eeb3a754c1a3a779110722d253c67f6e9fac900d7a89533ee3324751bcc9aaf44c3061cd18d77b7a0'
         };
     }
 
